2d0a68c4baf02fc00db8c91c1746fe1fb8751b6f
hero81 <106089581+hero81@users.noreply.github.com>
2023-01-02 19:10:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ for i in emails:
         # print(resp1.text)
         j = resp1.json()["data"]["plan_id"]
         # if j != 'None':  # 根据机场套餐id调整
-        if j != 'None' and str(j) != '1':  # 根据机场套餐id调整，当前机场套餐1为体验套餐
+        if j != 'None' and str(j) != '1':  # 根据机场套餐id调整，当前机场套餐id为1的是体验套餐
             sub_link = resp1.json()["data"]["subscribe_url"]
             traffic_download = resp1.json()["data"]["d"]
             traffic_total = resp1.json()["data"]["transfer_enable"]
